11aa79a871bd12f8657c2ff9fe7105cb8380a4dd
Andy Agus <redagus@gmail.com>
2020-05-19 20:06:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  array.push(element)[index_number-1]
+  array.push(element)[index_number]
 end
